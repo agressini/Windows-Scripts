3eebb114f71c33f79979fe093c5cf05667aa201f
agressini <36573756+agressini@users.noreply.github.com>
2019-02-22 12:44:32 -0300

    nuevos

---
+++ Replace-Attr.ps1
@@ -0,0 +1,13 @@
+﻿#$users = Get-ADUser -Filter * -Properties * | Select CN,DisplayName,DistinguishedName,GivenName,Name,Surname,UserPrincipalName,Info,sAMAccountName  
+
+foreach ($user in $users){
+
+    if ($user.Info -ieq "Staged=True")
+    {
+        Set-ADUser $user.sAMAccountName -Replace @{info=' '} -WhatIf  
+        $out = "Notes value was modified in user: " + $user.sAMAccountName |  Out-File C:\Users\sandrade-admin\Desktop\AXA_CHANGE_LOG.txt -Append
+    }
+
+}
+
+
---
+++ exporter.ps1
@@ -0,0 +1,6 @@
+﻿Get-ADUser -Filter * -Properties * | Select CN,DisplayName,DistinguishedName,GivenName,Name,Surname,UserPrincipalName,Info,sAMAccountName  | 
+Export-Csv -NoTypeInformation -Path C:\Users\sandrade-admin\Desktop\AXA_USERS_CHANGED_RUN2.csv
+Get-ADUser -Identity sandrade-admin -Properties * | Select UserPrincipalName,Info, sAMAccountName
+
+Set-ADUser sandrade-admin -Replace @{info=' '} | Out-File C:\Users\sandrade-admin\Desktop\AXA_CHANGE_LOG.txt
+$out = "Notes value was modified in user: " + $user.sAMAccountName |  Out-File C:\Users\sandrade-admin\Desktop\AXA_CHANGE_LOG.txt -Append
