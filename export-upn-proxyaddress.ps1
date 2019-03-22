d687cd70d7781ce874ac74d3ae395c5b72a5ed23
Antonino <36573756+agressini@users.noreply.github.com>
2019-03-22 10:58:12 -0300

    otro mas

---
+++ export-upn-proxyaddress.ps1
@@ -0,0 +1,13 @@
+﻿$users = Get-ADUser -filter * -Properties SamAccountName,UserPrincipalName,proxyAddresses 
+$CSV = "SamAccountName,UserPrincipalName,proxyAddresses `r`n"
+
+foreach ($user in $users){
+
+    $CSV += $user.SamAccountName + ","
+    $CSV += $user.UserPrincipalName + ","
+    $CSV += $user.proxyAddresses + "`r`n"
+
+}
+
+
+$CSV| Out-File $ENV:USERPROFILE\Desktop\Users.csv
