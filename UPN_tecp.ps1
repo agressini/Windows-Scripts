9e8e6354f13b8c38229ed8783210827167846743
Antonino <36573756+agressini@users.noreply.github.com>
2019-06-27 11:11:40 -0300

    correccion del log

--- UPN_tecp.ps1
+++ UPN_tecp.ps1
@@ -9,8 +9,8 @@ foreach ($user in $users)
     {
         $upn = $user.EmailAddress
         Set-ADUser $user.SamAccountName   -UserPrincipalName $upn
-        Write-Host "Cambiando el valor del usuario " $user.Username " Nuevo UPN: " $user.UPN
-        $csv = $user.Username + "," + $user.UPN 
+        Write-Host "Cambiando el valor del usuario " $user.SamAccountName " Nuevo UPN: " $upn
+        $csv = $user.SamAccountName + "," + $upn 
         $csv | Out-file -Append -FilePath .\reporte.csv
 
     }
